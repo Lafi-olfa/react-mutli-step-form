2633535e2ea058ac6b78351e453edcbee27a2a0b
olfalafi <olfalafi1998@gmail.com>
2025-11-25 21:35:17 +0100

    Wip:add next, go back buttons

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -1,10 +1,13 @@
 export default function Buttons() {
-    return(
-         <div className="flex justify-end mt-8 mx-4">
-            <button className="bg-blue-900 text-white py-3 px-4 rounded-lg hover:bg-blue-800 transition-colors font-medium">
-              Next Step
-            </button>
-          </div>
-    )
-    
+  return (
+    <div className="flex justify-between mt-8 mx-4">
+      <button className="text-gray-500 py-3 px-4 rounded-lg hover:bg-blue-800 transition-colors font-medium">
+        Go Back
+      </button>
+      <button className="bg-blue-900 text-white py-3 px-4 rounded-lg hover:bg-blue-800 transition-colors font-medium">
+        Next Step
+      </button>
+    </div>
+  )
+
 }
